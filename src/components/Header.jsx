bbbad9b4af047e0b3c62ce969845e552bb18b7d5
danilshatalov <danilshatalov96@gmail.com>
2022-10-29 15:02:46 +0200

    Add components

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <header className='header'>
+            <img src='https://cryptologos.cc/logos/uniswap-uni-logo.png'></img>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+function Profile() {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'></img>
+            </div>
+            <div>ava + discription</div>
+            <div>
+                my post
+                <div>
+                    new post
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
