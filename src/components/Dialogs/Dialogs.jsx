9a33defab59b1e5f1a66ac0b5a1a1876c8b48ae3
danilshatalov <danilshatalov96@gmail.com>
2022-11-01 13:40:33 +0100

    Add dialogs page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,41 @@ import style from './Dialogs.module.css'
 
 function Dialogs() {
     return (
-        <div>
-            Dialogs
+        <div className={style.dialogs}>
+            <div className={style.dialogsItem}>
+                <div className={style.dialog}>
+                    Robert
+                </div>
+                <div className={`${style.dialog} ${style.active}`}>
+                    Alexandra
+                </div>
+                <div className={style.dialog}>
+                    Elizabeth
+                </div>
+                <div className={style.dialog}>
+                    Margaret
+                </div>
+                <div className={style.dialog}>
+                    Phillip
+                </div>
+                <div className={style.dialog}>
+                    Megan
+                </div>
+            </div>
+            <div className={style.messages}>
+                <div className={style.message}>
+                    Hola
+                </div>
+                <div className={style.message}>
+                    Hey
+                </div>
+                <div className={style.message}>
+                    Lol
+                </div>
+                <div className={style.message}>
+                    Whats going on?
+                </div>
+            </div>
         </div>
     );
 }
